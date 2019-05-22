d7f10b95189f45f396b0b8a57d389fe97561ec6c
emperWang <544094478@qq.com>
2019-05-22 10:05:01 +0800

    添加dubbo配置，声明服务

--- pingoygou-parent/pingougou-sellergoods-service-impl/src/main/java/com/pinyougou/config/DubboProviderConfig.java
+++ pingoygou-parent/pingougou-sellergoods-service-impl/src/main/java/com/pinyougou/config/DubboProviderConfig.java
@@ -1,10 +1,8 @@
 package com.pinyougou.config;
 
 import com.alibaba.dubbo.config.*;
-import com.pinyougou.service.BrandService;
-import com.pinyougou.service.SpecificationService;
-import com.pinyougou.service.impl.BrandServiceImpl;
-import com.pinyougou.service.impl.SpecificationServiceImpl;
+import com.pinyougou.service.*;
+import com.pinyougou.service.impl.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
@@ -46,6 +44,102 @@ public class DubboProviderConfig {
         ServiceConfig<SpecificationService> serviceConfig = new ServiceConfig<>();
         serviceConfig.setInterface(SpecificationService.class);
         serviceConfig.setRef(specificationService);
+        serviceConfig.setRetries(3);
+        serviceConfig.setRegistry(registryConfig());
+        serviceConfig.setApplication(applicationConfig());
+        serviceConfig.setProtocol(protocolConfig());
+        serviceConfig.setMonitor(monitorConfig());
+        serviceConfig.export();
+        return serviceConfig;
+    }
+
+    @Bean
+    public ServiceConfig<SpecificationOptionService> specificationOptionServiceConfig(SpecificationOptionServiceImpl specificationService){
+        ServiceConfig<SpecificationOptionService> serviceConfig = new ServiceConfig<>();
+        serviceConfig.setInterface(SpecificationOptionService.class);
+        serviceConfig.setRef(specificationService);
+        serviceConfig.setRetries(3);
+        serviceConfig.setRegistry(registryConfig());
+        serviceConfig.setApplication(applicationConfig());
+        serviceConfig.setProtocol(protocolConfig());
+        serviceConfig.setMonitor(monitorConfig());
+        serviceConfig.export();
+        return serviceConfig;
+    }
+
+    @Bean
+    public ServiceConfig<GoodsDescService> goodsDescServiceConfig(GoodsDescServiceImpl goodsDescService){
+        ServiceConfig<GoodsDescService> serviceConfig = new ServiceConfig<>();
+        serviceConfig.setInterface(GoodsDescService.class);
+        serviceConfig.setRef(goodsDescService);
+        serviceConfig.setRetries(3);
+        serviceConfig.setRegistry(registryConfig());
+        serviceConfig.setApplication(applicationConfig());
+        serviceConfig.setProtocol(protocolConfig());
+        serviceConfig.setMonitor(monitorConfig());
+        serviceConfig.export();
+        return serviceConfig;
+    }
+
+    @Bean
+    public ServiceConfig<GoodsService> goodsServiceConfig(GoodsServiceImpl goodsService){
+        ServiceConfig<GoodsService> serviceConfig = new ServiceConfig<>();
+        serviceConfig.setInterface(GoodsService.class);
+        serviceConfig.setRef(goodsService);
+        serviceConfig.setRetries(3);
+        serviceConfig.setRegistry(registryConfig());
+        serviceConfig.setApplication(applicationConfig());
+        serviceConfig.setProtocol(protocolConfig());
+        serviceConfig.setMonitor(monitorConfig());
+        serviceConfig.export();
+        return serviceConfig;
+    }
+    @Bean
+    public ServiceConfig<ItemCatService> itemCatServiceConfig(ItemCatServiceImpl itemCatService){
+        ServiceConfig<ItemCatService> serviceConfig = new ServiceConfig<>();
+        serviceConfig.setInterface(ItemCatService.class);
+        serviceConfig.setRef(itemCatService);
+        serviceConfig.setRetries(3);
+        serviceConfig.setRegistry(registryConfig());
+        serviceConfig.setApplication(applicationConfig());
+        serviceConfig.setProtocol(protocolConfig());
+        serviceConfig.setMonitor(monitorConfig());
+        serviceConfig.export();
+        return serviceConfig;
+    }
+
+    @Bean
+    public ServiceConfig<ItemService> itemServiceConfig(ItemServiceImpl itemService){
+        ServiceConfig<ItemService> serviceConfig = new ServiceConfig<>();
+        serviceConfig.setInterface(ItemService.class);
+        serviceConfig.setRef(itemService);
+        serviceConfig.setRetries(3);
+        serviceConfig.setRegistry(registryConfig());
+        serviceConfig.setApplication(applicationConfig());
+        serviceConfig.setProtocol(protocolConfig());
+        serviceConfig.setMonitor(monitorConfig());
+        serviceConfig.export();
+        return serviceConfig;
+    }
+
+    @Bean
+    public ServiceConfig<SellerService> sellerServiceConfig(SellerServiceImpl sellerService){
+        ServiceConfig<SellerService> serviceConfig = new ServiceConfig<>();
+        serviceConfig.setInterface(SellerService.class);
+        serviceConfig.setRef(sellerService);
+        serviceConfig.setRetries(3);
+        serviceConfig.setRegistry(registryConfig());
+        serviceConfig.setApplication(applicationConfig());
+        serviceConfig.setProtocol(protocolConfig());
+        serviceConfig.setMonitor(monitorConfig());
+        serviceConfig.export();
+        return serviceConfig;
+    }
+    @Bean
+    public ServiceConfig<TypeTemplateService> typeTemplateServiceConfig(TypeTemplateServiceImpl typeTemplateService){
+        ServiceConfig<TypeTemplateService> serviceConfig = new ServiceConfig<>();
+        serviceConfig.setInterface(TypeTemplateService.class);
+        serviceConfig.setRef(typeTemplateService);
         serviceConfig.setRetries(3);
         serviceConfig.setRegistry(registryConfig());
         serviceConfig.setApplication(applicationConfig());
