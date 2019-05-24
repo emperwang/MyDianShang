24124d2cbe8622d3af35d82f4932b37e4226e1e4
emperWang <544094478@qq.com>
2019-05-24 11:08:53 +0800

    move dubboConfig to config.dubbo package

--- pingoygou-parent/pinyougou-manager-web/src/main/java/com/pinyougou/config/DubboConsumerConfig.java
+++
@@ -1,9 +0,0 @@
-package com.pinyougou.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-
-@Configuration
-@ImportResource(value = {"dubbo-consumer.xml"})
-public class DubboConsumerConfig {
-}
