5016b9d12799763b17831c203eece1f9771678ff
emperWang <544094478@qq.com>
2019-05-24 12:25:15 +0800

    显示用户名功能

--- pingoygou-parent/pinyougou-manager-web/src/main/java/com/pinyougou/Controller/LoginController.java
+++ pingoygou-parent/pinyougou-manager-web/src/main/java/com/pinyougou/Controller/LoginController.java
@@ -1,7 +1,13 @@
 package com.pinyougou.Controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/login")
@@ -15,5 +21,15 @@ public class LoginController {
     @RequestMapping("/login-error.do")
     public String loginError(){
         return "login_error";
+    }
+
+    @RequestMapping(value = "/loginName.do",method = RequestMethod.GET)
+    @ResponseBody
+    public Map getName(){
+        //用户名
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        Map<String,String> map = new HashMap<>();
+        map.put("loginName",name);
+        return map;
     }
 }
---
+++ pingoygou-parent/pinyougou-manager-web/src/main/resources/resources/js/controller/indexController.js
@@ -0,0 +1,8 @@
+app.controller("indexController",function ($scope,loginService) {
+
+    $scope.showLoginName=function () {
+        loginService.loginName().success(function (data) {
+            $scope.loginName=data.loginName;
+        });
+    }
+})
