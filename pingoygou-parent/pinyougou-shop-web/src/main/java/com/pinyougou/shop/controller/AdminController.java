ebdf61eafcba6883b159e674c0140d033f6fa2c4
emperWang <544094478@qq.com>
2019-05-24 16:01:23 +0800

    admin页面的跳转函数

--- pingoygou-parent/pinyougou-shop-web/src/main/java/com/pinyougou/shop/controller/AdminController.java
+++ pingoygou-parent/pinyougou-shop-web/src/main/java/com/pinyougou/shop/controller/AdminController.java
@@ -2,9 +2,35 @@ package com.pinyougou.shop.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+
+    @GetMapping("/index.do")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/goods-edit.do")
+    public String goods_edit(){
+        return "goods_edit";
+    }
+
+    @GetMapping("/home.do")
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("/password.do")
+    public String password(){
+        return "password";
+    }
+
+    @GetMapping("/seller.do")
+    public String seller(){
+        return "index";
+    }
 }
