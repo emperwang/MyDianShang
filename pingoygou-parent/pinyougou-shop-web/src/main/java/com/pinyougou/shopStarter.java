0321acae2a0ef90770980650285fbb17b2526406
emperWang <544094478@qq.com>
2019-05-24 13:03:12 +0800

    卖家web模块

---
+++ pingoygou-parent/pinyougou-shop-web/src/main/java/com/pinyougou/shopStarter.java
@@ -0,0 +1,11 @@
+package com.pinyougou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class shopStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(shopStarter.class,args);
+    }
+}
