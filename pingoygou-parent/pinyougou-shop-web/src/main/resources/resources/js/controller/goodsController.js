22ada8c141b7a35e7f69678db8eec16ebe4d0787
emperWang <544094478@qq.com>
2019-05-29 15:31:43 +0800

    商品审核的前台展示

--- pingoygou-parent/pinyougou-shop-web/src/main/resources/resources/js/controller/goodsController.js
+++ pingoygou-parent/pinyougou-shop-web/src/main/resources/resources/js/controller/goodsController.js
@@ -85,7 +85,7 @@ app.controller('goodsController' ,function($scope,$controller,goodsService,itemC
 	$scope.search=function(page,rows){			
 		goodsService.search(page,rows,$scope.searchEntity).success(
 			function(response){
-				$scope.list=response.rows;	
+				$scope.list=response.results;
 				$scope.paginationConf.totalItems=response.total;//更新总记录数
 			}			
 		);
