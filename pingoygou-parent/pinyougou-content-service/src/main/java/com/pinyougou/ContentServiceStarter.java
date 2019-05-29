81b193caa6f54cfeb05181ffc8661f590edf7ca4
emperWang <544094478@qq.com>
2019-05-29 20:07:00 +0800

    add staterMain

---
+++ pingoygou-parent/pinyougou-content-service/src/main/java/com/pinyougou/ContentServiceStarter.java
@@ -0,0 +1,11 @@
+package com.pinyougou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentServiceStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(ContentServiceStarter.class,args);
+    }
+}
