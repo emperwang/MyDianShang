a737d8975a0ada4de79e8b67a5e9ce0f7f8d6e61
emperWang <544094478@qq.com>
2019-05-29 20:07:46 +0800

    rename rows to results

--- pingoygou-parent/pinyougou-manager-web/src/main/resources/resources/js/controller/contentCategoryController.js
+++ pingoygou-parent/pinyougou-manager-web/src/main/resources/resources/js/controller/contentCategoryController.js
@@ -16,7 +16,7 @@ app.controller('contentCategoryController' ,function($scope,$controller   ,conte
 	$scope.findPage=function(page,rows){			
 		contentCategoryService.findPage(page,rows).success(
 			function(response){
-				$scope.list=response.rows;	
+				$scope.list=response.results;
 				$scope.paginationConf.totalItems=response.total;//更新总记录数
 			}			
 		);
@@ -71,7 +71,7 @@ app.controller('contentCategoryController' ,function($scope,$controller   ,conte
 	$scope.search=function(page,rows){			
 		contentCategoryService.search(page,rows,$scope.searchEntity).success(
 			function(response){
-				$scope.list=response.rows;	
+				$scope.list=response.results;
 				$scope.paginationConf.totalItems=response.total;//更新总记录数
 			}			
 		);
