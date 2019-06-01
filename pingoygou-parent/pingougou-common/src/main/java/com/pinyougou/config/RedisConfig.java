32992cf69f367a3464d370820354468d13bcd3d1
emperWang <544094478@qq.com>
2019-06-01 16:24:56 +0800

    add redis in common

---
+++ pingoygou-parent/pingougou-common/src/main/java/com/pinyougou/config/RedisConfig.java
@@ -0,0 +1,25 @@
+package com.pinyougou.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.io.Serializable;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<Serializable,Serializable> redisTemplate(RedisConnectionFactory  redisConnectionFactory){
+        RedisTemplate<Serializable, Serializable> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
+        redisTemplate.setEnableTransactionSupport(true);
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+}
