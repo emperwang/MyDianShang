dee3a8a522adade7decb8d2f195617a9aac70749
emperWang <544094478@qq.com>
2019-06-06 18:00:42 +0800

    高亮前台实现

--- pingoygou-parent/pinyougou-search-web/src/main/resources/resources/js/base.js
+++ pingoygou-parent/pinyougou-search-web/src/main/resources/resources/js/base.js
@@ -1,1 +1,12 @@
 var app=angular.module('pinyougou',[]);
+
+// 定义一个信任组件，表示解析js或者html代码
+// 前台搜索高亮显示使用到
+app.filter("trustHtml",['$sce',function ($sce) {
+/*    return function (date) {
+        $sce.trustAsHtml(date);
+    }*/
+    return function(data){
+        return $sce.trustAsHtml(data);
+    }
+} ]);
