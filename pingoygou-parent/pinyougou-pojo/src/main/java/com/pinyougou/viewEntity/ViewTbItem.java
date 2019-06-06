fe2be98b858d063e2022d176f1c896953fa227b2
emperWang <544094478@qq.com>
2019-06-06 12:47:50 +0800

    add solr entity

---
+++ pingoygou-parent/pinyougou-pojo/src/main/java/com/pinyougou/viewEntity/ViewTbItem.java
@@ -0,0 +1,282 @@
+package com.pinyougou.viewEntity;
+
+import org.apache.solr.client.solrj.beans.Field;
+import org.springframework.data.solr.core.mapping.Dynamic;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Map;
+
+public class ViewTbItem implements Serializable {
+    @Field("id")
+    private Long id;
+    @Field("item_title")
+    private String title;
+
+    private String sellPoint;
+    @Field("item_price")
+    private Double price;
+
+    private Integer stockCount;
+
+    private Integer num;
+
+    private String barcode;
+    @Field("")
+    private String image;
+
+    private Long categoryid;
+
+    private String status;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private String itemSn;
+
+    private Double costPirce;
+
+    private Double marketPrice;
+
+    private String isDefault;
+    @Field("")
+    private Long goodsId;
+
+    private String sellerId;
+
+    private String cartThumbnail;
+    @Field("item_category")
+    private String category;
+    @Field("item_brand")
+    private String brand;
+
+    private String spec;
+    @Field("item_seller")
+    private String seller;
+    @Dynamic
+    @Field("item_spec_*")
+    public Map<String,String> mapSpec;
+
+    public Map<String, String> getMapSpec() {
+        return mapSpec;
+    }
+
+    public void setMapSpec(Map<String, String> mapSpec) {
+        this.mapSpec = mapSpec;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title == null ? null : title.trim();
+    }
+
+    public String getSellPoint() {
+        return sellPoint;
+    }
+
+    public void setSellPoint(String sellPoint) {
+        this.sellPoint = sellPoint == null ? null : sellPoint.trim();
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getStockCount() {
+        return stockCount;
+    }
+
+    public void setStockCount(Integer stockCount) {
+        this.stockCount = stockCount;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode == null ? null : barcode.trim();
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image == null ? null : image.trim();
+    }
+
+    public Long getCategoryid() {
+        return categoryid;
+    }
+
+    public void setCategoryid(Long categoryid) {
+        this.categoryid = categoryid;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status == null ? null : status.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getItemSn() {
+        return itemSn;
+    }
+
+    public void setItemSn(String itemSn) {
+        this.itemSn = itemSn == null ? null : itemSn.trim();
+    }
+
+    public Double getCostPirce() {
+        return costPirce;
+    }
+
+    public void setCostPirce(Double costPirce) {
+        this.costPirce = costPirce;
+    }
+
+    public Double getMarketPrice() {
+        return marketPrice;
+    }
+
+    public void setMarketPrice(Double marketPrice) {
+        this.marketPrice = marketPrice;
+    }
+
+    public String getIsDefault() {
+        return isDefault;
+    }
+
+    public void setIsDefault(String isDefault) {
+        this.isDefault = isDefault == null ? null : isDefault.trim();
+    }
+
+    public Long getGoodsId() {
+        return goodsId;
+    }
+
+    public void setGoodsId(Long goodsId) {
+        this.goodsId = goodsId;
+    }
+
+    public String getSellerId() {
+        return sellerId;
+    }
+
+    public void setSellerId(String sellerId) {
+        this.sellerId = sellerId == null ? null : sellerId.trim();
+    }
+
+    public String getCartThumbnail() {
+        return cartThumbnail;
+    }
+
+    public void setCartThumbnail(String cartThumbnail) {
+        this.cartThumbnail = cartThumbnail == null ? null : cartThumbnail.trim();
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category == null ? null : category.trim();
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand == null ? null : brand.trim();
+    }
+
+    public String getSpec() {
+        return spec;
+    }
+
+    public void setSpec(String spec) {
+        this.spec = spec == null ? null : spec.trim();
+    }
+
+    public String getSeller() {
+        return seller;
+    }
+
+    public void setSeller(String seller) {
+        this.seller = seller == null ? null : seller.trim();
+    }
+
+    @Override
+    public String toString() {
+        return "ViewTbItem{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", sellPoint='" + sellPoint + '\'' +
+                ", price=" + price +
+                ", stockCount=" + stockCount +
+                ", num=" + num +
+                ", barcode='" + barcode + '\'' +
+                ", image='" + image + '\'' +
+                ", categoryid=" + categoryid +
+                ", status='" + status + '\'' +
+                ", createTime=" + createTime +
+                ", updateTime=" + updateTime +
+                ", itemSn='" + itemSn + '\'' +
+                ", costPirce=" + costPirce +
+                ", marketPrice=" + marketPrice +
+                ", isDefault='" + isDefault + '\'' +
+                ", goodsId=" + goodsId +
+                ", sellerId='" + sellerId + '\'' +
+                ", cartThumbnail='" + cartThumbnail + '\'' +
+                ", category='" + category + '\'' +
+                ", brand='" + brand + '\'' +
+                ", spec='" + spec + '\'' +
+                ", seller='" + seller + '\'' +
+                ", mapSpec=" + mapSpec +
+                '}';
+    }
+}
