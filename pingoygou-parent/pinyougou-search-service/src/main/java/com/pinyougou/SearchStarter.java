0b500a3786a3cde3cc8b080c4f9b3fb4a9fa4888
emperWang <544094478@qq.com>
2019-06-06 14:42:12 +0800

    search interface,service,web; base project

---
+++ pingoygou-parent/pinyougou-search-service/src/main/java/com/pinyougou/SearchStarter.java
@@ -0,0 +1,11 @@
+package com.pinyougou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(SearchStarter.class,args);
+    }
+}
