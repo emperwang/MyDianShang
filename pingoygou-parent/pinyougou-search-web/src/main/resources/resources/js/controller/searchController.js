33bd55e747c926ff2b1c9bc920d69fa0742b205c
emperWang <544094478@qq.com>
2019-06-07 12:55:48 +0800

    搜索条件添加

--- pingoygou-parent/pinyougou-search-web/src/main/resources/resources/js/controller/searchController.js
+++ pingoygou-parent/pinyougou-search-web/src/main/resources/resources/js/controller/searchController.js
@@ -1,10 +1,16 @@
 app.controller("searchController",function ($scope,$controller,searchService) {
     $controller('publicController',{$scope:$scope});//继承
-
+    $scope.searchMap={'keywords':'','category':'','brand':'','spec':{}};
     $scope.searchItem=function () {
         searchService.searchItem($scope.searchMap).success(function (response) {
             $scope.resultMap = response;
         });
     }
-
+    $scope.addSearchItem=function (key,value) {
+        if (key == 'category' || key == 'brand'){  // 说明用户点击的是品牌和分类
+            $scope.searchMap[key] = value;
+        }else{ // 否则是规格选项
+            $scope.searchMap.spec[key]=value;
+        }
+    }
 });
